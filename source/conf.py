a4968e4938e2952441844e824dd8a8145a15a4ce
pfnie <pfnie@yahoo.com>
2022-06-16 15:17:36 +0800

    done

--- source/conf.py
+++ source/conf.py
@@ -73,4 +73,12 @@ html_show_sphinx = False
 #language = None
 
 html_theme_options = {
-    'display_version': False}
+    'display_version': False
+}
+
+html_context = {
+"display_github": False, # Add 'Edit on Github' link instead of 'View page source'
+"last_updated": True,
+"commit": False,
+}
+
